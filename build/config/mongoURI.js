1c95717a8ce636363dcb8f4eeb363f44ff355049
hervé BAKONGO <herdiebakongo@gmail.com>
2022-05-24 09:35:52 +0100

    updating 01

--- build/config/mongoURI.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-exports.db = 'mongodb://localhost:27017/HR';
-// 'mongodb+srv://vignettedb:Hood1097@clusterdiams0.kxafd.gcp.mongodb.net/HR?retryWrites=true&w=majority';
-// 
-//# sourceMappingURL=mongoURI.js.map
